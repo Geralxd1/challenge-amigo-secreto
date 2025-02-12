152c8b96f80ce871a971bcf36d9a70878a686295
Geral <105967573+Geralxd1@users.noreply.github.com>
2025-02-12 17:18:13 -0500

    primeras funciones

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+var listaAmigos = [];
+const listaAmigosHTML = document.getElementById('listaAmigos');
+const amigoNombre = document.getElementById('amigo')
+function agregarAmigo() {
+    let nombreAmigo = amigoNombre.value;
+    if (validarNombre(nombreAmigo)) {
+        listaAmigos.push(nombreAmigo);
+        while (listaAmigosHTML.firstChild) {
+            listaAmigosHTML.removeChild(listaAmigosHTML.firstChild);
+        }
+        listaAmigos.forEach((amigo) => {
+            const elemento = document.createElement('div');
+            elemento.textContent = amigo;
+            elemento.style.margin = '10px';
+            listaAmigosHTML.appendChild(elemento);
+        });
+        amigoNombre.value = ''
+    }
+    else
+        alert('Ingrese un nombre valido')
+}
+
+function validarNombre(nombre) {
+    if (nombre.trim() === '')
+        return false
+    return true;
+}
+function sortearAmigo() {
+    if (listaAmigos.length < 2) {
+        alert('Necesita al menos dos nombres para realizar el sorteo')
+    }
+}
