d43e9fc3824a7597c0217c7bb038a258ff801b83
Geral <105967573+Geralxd1@users.noreply.github.com>
2025-02-14 13:16:19 -0500

    Version funcional

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var listaAmigos = [];
 const listaAmigosHTML = document.getElementById('listaAmigos');
-const amigoNombre = document.getElementById('amigo')
+const amigoNombre = document.getElementById('amigo');
+const amigoGanador = document.getElementById('resultado');
+const botonSorteo = document.getElementById('botonSorteo');
+
+var hijos = listaAmigosHTML.children;
+var idGanador = -1;
+
 function agregarAmigo() {
     let nombreAmigo = amigoNombre.value;
     if (validarNombre(nombreAmigo)) {
@@ -12,6 +18,8 @@ function agregarAmigo() {
             const elemento = document.createElement('div');
             elemento.textContent = amigo;
             elemento.style.margin = '10px';
+            elemento.style.border = '1px solid red';
+            elemento.style.padding = '5px 10px';
             listaAmigosHTML.appendChild(elemento);
         });
         amigoNombre.value = ''
@@ -25,8 +33,30 @@ function validarNombre(nombre) {
         return false
     return true;
 }
+
+function randomEntre(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function sortearAmigo() {
+    console.log('============CLICK==============')
     if (listaAmigos.length < 2) {
         alert('Necesita al menos dos nombres para realizar el sorteo')
+        return
     }
+
+    if (idGanador > -1) {
+        hijos[idGanador].style.background = 'none';
+    }
+    let idAmigo = randomEntre(0, listaAmigos.length - 1);
+    if(idGanador != idAmigo)
+    {
+        idGanador = idAmigo;
+        amigoGanador.textContent = `Tu amigo secreto es: ${listaAmigos[idAmigo]}`;
+        hijos[idAmigo].style.background = 'yellow';
+        hijos[idAmigo].style.color = 'blue';
+    }else{
+        sortearAmigo()
+    }
+    
 }
